0a7057133320b6faaf3e2945874b3890cd76cb37
roshan <roshanshaji023@gmail.com>
2022-05-12 18:23:51 +0530

    l

---
+++ app/src/main/java/com/example/h2ofit/Login.java
@@ -0,0 +1,22 @@
+package com.example.h2ofit;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+
+import android.graphics.Color;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.google.android.material.button.MaterialButton;
+
+public class Login extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+
+    }
+}
---
+++ app/src/main/java/com/example/h2ofit/SplashScreen.java
@@ -0,0 +1,30 @@
+package com.example.h2ofit;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+
+public class SplashScreen extends AppCompatActivity {
+    //splash screen
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splashscreen);
+
+        new Handler().postDelayed(new Runnable() {
+
+
+            @Override
+            public void run() {
+                // This method will be executed once the timer is over
+                Intent i = new Intent(SplashScreen.this, Login.class);
+                startActivity(i);
+                finish();//to not to return to splash screen on back touch
+            }
+        }, 2000);
+
+    }
+}
