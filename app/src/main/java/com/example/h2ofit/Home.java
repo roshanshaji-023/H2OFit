9d45ca3de12f22d5151580975136d840b36d01a5
roshan <roshanshaji023@gmail.com>
2022-05-31 21:42:47 +0530

    .

--- app/src/main/java/com/example/h2ofit/Home.java
+++ app/src/main/java/com/example/h2ofit/Home.java
@@ -2,14 +2,31 @@ package com.example.h2ofit;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ImageButton;
 
 public class Home extends AppCompatActivity {
-
+    private ImageButton profile_button;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+        /*for opening profile activity*/
+        profile_button=(ImageButton) findViewById(R.id.imageButton8);
+        profile_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openPerzonalize();
+
+            }
+        });
+
     }
+    public void openPerzonalize(){
+        Intent intent=new Intent(this, profile.class);
+        startActivity(intent);
+    }
+
 }
